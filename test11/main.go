8da9c928d72dc07da1123e1f9f72c4c88207bcf1
Your Name <dukelu999@gmail.com>
2025-11-20 20:44:43 +0700

    test11

--- test11/main.go
+++ test11/main.go
@@ -5,18 +5,23 @@ import (
 	"sync"
 )
 
-func print_number(index int64, wg *sync.WaitGroup) {
+func print_number(index int64, wg *sync.WaitGroup, mu *sync.Mutex) {
 	defer wg.Done()
+	mu.Lock()
 	for i := 0; i < 10; i++ {
 		fmt.Println("index:", index, "i:", i)
+
 	}
+	mu.Unlock()
 }
 
 func main() {
 	var wg sync.WaitGroup
+	var mu sync.Mutex
+
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
-		go print_number(int64(i), &wg)
+		go print_number(int64(i), &wg, &mu)
 	}
 	wg.Wait()
 	fmt.Println("All goroutines finished.")
