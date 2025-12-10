d1b729f732224ed127b921de08fc08227908a927
Your Name <dukelu999@gmail.com>
2025-12-10 22:10:46 +0700

    test14

--- test14/main.go
+++ test14/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/sirupsen/logrus"
 )
 
-func main() {
+func initLogger() {
+	write, _ := os.OpenFile("test.log",
+		os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	logrus.SetOutput(io.MultiWriter(os.Stdout, write))
 	logrus.SetLevel(logrus.TraceLevel)
+	logrus.SetReportCaller(true)
+}
+func main() {
+	initLogger()
 	logrus.Trace("trace msg")
 	logrus.Debug("debug msg")
 	logrus.Info("info msg")
@@ -15,11 +25,11 @@ func main() {
 	//logrus.Panic("panic msg")
 	logrus.SetReportCaller(true)
 	logrus.Info("info msg with caller")
-
 	log := logrus.WithFields(logrus.Fields{
 		"user_id": 681200,
 		"action":  "test",
 		"time":    "2025-12-9",
 	})
 	log.Info("msg")
+
 }
